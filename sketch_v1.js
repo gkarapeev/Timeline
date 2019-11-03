24c9ab8e13dd40fbd7c4c6d29583d556e53ffce1
Georgi <gkarapeev@gmail.com>
2019-11-04 00:53:01 +0200

    Implement Webpack & Babel

--- sketch_v1.js
+++ sketch_v1.js
@@ -1,85 +1,34 @@
-// Canvas
-let height = 700;
-let width = 1200;
+import generatePoints from './points';
+import './styles.scss';
 
-// Timeline
-let timelineLength = width - 100;
-let timelineHeight = 3;
+// // Canvas
+// let screenHeight = 700;
+// let screenWidth = 1200;
 
-// Points
-let points_Y = 340;
-
-let pointA = {
-  x: 0,
-  importance: 1
-}
-
-let pointB = {
-  x: 1000,
-  importance: 2
-}
-
-let pointC = {
-  x: 800,
-  importance: 3
-}
-
-let pointD = {
-  x: -900,
-  importance: 4
-}
-
-let pointE = {
-  x: -300,
-  importance: 5
-}
+// // Timeline
+// let timelineLength = screenWidth - 100;
+// let timelineHeight = 3;
 
 
-function setup() {
-  let mainCanvas = createCanvas(width, height);
-  mainCanvas.parent("#sketch-container");
-  rectMode(CENTER);
-}
+// function setup() {
+//   let mainCanvas = createCanvas(screenWidth, screenHeight);
+//   mainCanvas.parent("#sketch-container");
+//   rectMode(CENTER);
+// }
 
-function draw() {
-  background(50, 50, 50);
-  fill(255);
-  noStroke();
-  rect(width / 2, height / 2, timelineLength, timelineHeight, 2.5);
+// function draw() {
+//   background(50, 50, 50);
+//   fill(255);
+//   noStroke();
 
-  // Points
-  stroke(250, 0, 250);
-  strokeWeight(10);
+//   // Points
+//   stroke(250, 0, 250);
+//   strokeWeight(10);
+// }
 
-  if (calcPointX(pointA))
-  point(calcPointX(pointA), points_Y);
-  point(calcPointX(pointB), points_Y);
-  point(calcPointX(pointC), points_Y);
-  point(calcPointX(pointD), points_Y);
-  point(calcPointX(pointE), points_Y);
-}
+// function mouseWheel(event) {
+//   let speed = event.delta;
 
-function mouseWheel(event) {
-  let speed = event.delta;
-
-  timelineLength += speed / 2;
-
-  // Cap shrinking at 300
-  if (timelineLength < 300) {
-    timelineLength = 300;
-  }
-
-  // Cap expanding at 1200
-  if (timelineLength > 1200) {
-    timelineLength = 1200;
-  }
-
-  // block page scrolling
-  return false;
-}
-
-function calcPointX(ref) {
-  let lineEnds = timelineLength / 2;
-  let distanceFromCenter = map(ref.x, -1000, 1000, -lineEnds, lineEnds);
-  return width / 2 + distanceFromCenter;
-}
+//   // block page scrolling
+//   return false;
+// }
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+
+module.exports = {
+  entry: './sketch.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  }
+}
