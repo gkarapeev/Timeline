5c1c6e5a02819c0ea985f87cf06c13eeaaa37a17
Georgi Karapeev <gkarapeev@activtrades.com>
2024-10-16 10:28:05 +0300

    chore: rename dist to docs - for pages

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
 	entry: "./src/index.ts",
 	output: {
-		path: path.resolve(__dirname, "dist"),
+		path: path.resolve(__dirname, "docs"),
 		filename: "bundle.js",
 	},
 	mode: "development",
